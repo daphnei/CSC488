b05fe2cbdeb792e05af4a4d8831a275cf7335850
Daphne Ippolito <daphneipp@gmail.com>
2015-02-02 13:37:38 -0500

    test script works. Still trying to fix arguments

--- A2/RUNALLTESTS.sh
+++ A2/RUNALLTESTS.sh
@@ -1,9 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 
-echo POOP
+#Check for the correct usage
 
-if [ "$#" != 3]; then
-	echo "RUNALLTESTS.sh compiler.jar passing_dir failing_dir"
+#while getopts ":pf" opt
+#do
+
+#done
+
+if [ "$#" != 3 ]
+then
+	echo "Usage: sh RUNALLTESTS.sh [-pf] compiler.jar test_dir"
+	echo "Use -p if tests in test_dir are intended to compile without error."
+	echo "Use -f if tests in test_dir should fail when compiled."
+	echo "-p is used as default"
+
 	exit 1
 fi
 
@@ -11,16 +21,25 @@ COMPILER=$1
 PASSING_DIR=$2
 FAILING_DIR=$3
 
+#Iterate over all of the .488 files in the passing directory.
 for file in $(find $PASSING_DIR -name '*.488')
 do
-	java -jar $COMPILER $file | grep "Error"
-	echo $?	
+	#Try to compile the file.
+	result="$(java -jar $COMPILER $file 2>&1 )"
+	# 2>&1
+	#echo "$result"
 
-	#if $? 
-	#then
-	#	echo "The file with the error  was" $file
-	#fi
+	#If the compilation output contains the word error, print the error information.
+	grep -qv "Error" <<< $result
+	if [ ! $? -eq 0 ]
+	then
+		echo "Error in file: $file"
+		echo "$result"
+		echo
+	fi	
 done
+
+#Iterate over all of the .488 files in the failing directory,
 
 exit 0
 
