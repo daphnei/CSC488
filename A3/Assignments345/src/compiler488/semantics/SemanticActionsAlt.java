0ace1962fd178e0af2b4b008309185043e692240
Daphne <daphneipp@gmail.com>
2015-02-22 22:48:07 -0500

    semantic actions class

---
+++ A3/Assignments345/src/compiler488/semantics/SemanticActionsAlt.java
@@ -0,0 +1,188 @@
+package compiler488.semantics;
+
+public class SemanticActions {
+	public static void check_00(IVisitable visitable) {
+
+	}
+
+	public static void check_01(IVisitable visitable) {
+
+	}
+
+	public static void check_02(IVisitable visitable) {
+
+	}
+
+	public static void check_03(IVisitable visitable) {
+
+	}
+
+	public static void check_04(IVisitable visitable) {
+
+	}
+
+	public static void check_05(IVisitable visitable) {
+
+	}
+
+	public static void check_06(IVisitable visitable) {
+
+	}
+
+	public static void check_07(IVisitable visitable) {
+
+	}
+
+	public static void check_08(IVisitable visitable) {
+
+	}
+
+	public static void check_09(IVisitable visitable) {
+
+	}
+
+	public static void check_10(IVisitable visitable) {
+
+	}
+
+	public static void check_11(IVisitable visitable) {
+
+	}
+
+	public static void check_12(IVisitable visitable) {
+
+	}
+
+	public static void check_13(IVisitable visitable) {
+
+	}
+
+	public static void check_14(IVisitable visitable) {
+
+	}
+
+	public static void check_15(IVisitable visitable) {
+
+	}
+
+	public static void check_16(IVisitable visitable) {
+
+	}
+
+	public static void check_17(IVisitable visitable) {
+
+	}
+
+	public static void check_18(IVisitable visitable) {
+
+	}
+
+	public static void check_19(IVisitable visitable) {
+
+	}
+
+	public static void check_20(IVisitable visitable) {
+
+	}
+
+	public static void check_21(IVisitable visitable) {
+
+	}
+
+	public static void check_22(IVisitable visitable) {
+
+	}
+
+	public static void check_23(IVisitable visitable) {
+
+	}
+
+	public static void check_24(IVisitable visitable) {
+
+	}
+
+	public static void check_25(IVisitable visitable) {
+
+	}
+
+	public static void check_26(IVisitable visitable) {
+
+	}
+
+	public static void check_27(IVisitable visitable) {
+
+	}
+
+	public static void check_28(IVisitable visitable) {
+
+	}
+
+	public static void check_29(IVisitable visitable) {
+
+	}
+
+	public static void check_30(IVisitable visitable) {
+
+	}
+
+	public static void check_31(IVisitable visitable) {
+
+	}
+
+	public static void check_32(IVisitable visitable) {
+
+	}
+
+	public static void check_33(IVisitable visitable) {
+
+	}
+
+	public static void check_34(IVisitable visitable) {
+
+	}
+
+	public static void check_35(IVisitable visitable) {
+
+	}
+
+	public static void check_36(IVisitable visitable) {
+
+	}
+
+	public static void check_37(IVisitable visitable) {
+
+	}
+
+	public static void check_38(IVisitable visitable) {
+
+	}
+
+	public static void check_39(IVisitable visitable) {
+
+	}
+
+	public static void check_40(IVisitable visitable) {
+
+	}
+
+	public static void check_41(IVisitable visitable) {
+
+	}
+
+	public static void check_42(IVisitable visitable) {
+
+	}
+
+	public static void check_43(IVisitable visitable) {
+
+	}
+
+	public static void check_44(IVisitable visitable) {
+
+	}
+
+	public static void check_45(IVisitable visitable) {
+
+	}
+
+}
